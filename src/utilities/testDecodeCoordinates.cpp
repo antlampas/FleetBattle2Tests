2b192ba65ef52f89ca2bf1b6b150d6ee747c8932
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-15 19:22:41 +0200

    -

--- src/utilities/testDecodeCoordinates.cpp
+++ src/utilities/testDecodeCoordinates.cpp
@@ -1,3 +1,12 @@
 #include "catch2/catch_test_macros.hpp"
 
 #include "utilities.hpp"
+
+TEST_CASE("Decode coordinates", "[decodeCoordinates]"){
+    for(char column: {'a','b','c','d','e','f','g','h','i','j'})
+        for(int row: {1,..,10})
+        {
+            REQUIRE(decodeCoordinates(std::string(1,column)+std::itos(row)) == decodedCoordinates(static_cast<int>(column)-97,row));
+            REQUIRE(decodeCoordinates(std::string(1,column-32)+std::itos(row)) == decodedCoordinates(static_cast<int>(column)-65,row));
+        }
+}
