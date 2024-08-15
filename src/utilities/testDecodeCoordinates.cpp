d6f2cefbe952a3bd0fb0f8c465d4b563108e099b
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-15 19:57:08 +0200

    -

--- src/utilities/testDecodeCoordinates.cpp
+++ src/utilities/testDecodeCoordinates.cpp
@@ -9,8 +9,15 @@ TEST_CASE("Decode coordinates", "[decodeCoordinates]"){
     {
         for(int row: {1,2,3,4,5,6,7,8,9,10})
         {
-            REQUIRE(decodeCoordinates(std::string(1,column)+std::to_string(row))    == DecodedCoordinates{static_cast<int>(column)-97,row});
-            REQUIRE(decodeCoordinates(std::string(1,column-32)+std::to_string(row)) == DecodedCoordinates{static_cast<int>(column)-65,row});
+            REQUIRE(decodeCoordinates(std::string(1,column)+std::to_string(row)) == DecodedCoordinates{static_cast<int>(column)-97,row});
+        }
+    }
+
+    for(char column: {'A','B','C','D','E','F','G','H','I','J'})
+    {
+        for(int row: {1,2,3,4,5,6,7,8,9,10})
+        {
+            REQUIRE(decodeCoordinates(std::string(1,column)+std::to_string(row)) == DecodedCoordinates{static_cast<int>(column)-65,row});
         }
     }
 }
