9844f7abc30d8187bc2efd5518fe43de403c5d5e
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-15 19:57:43 +0200

    -

--- src/utilities/testDecodeCoordinates.cpp
+++ src/utilities/testDecodeCoordinates.cpp
@@ -12,7 +12,6 @@ TEST_CASE("Decode coordinates", "[decodeCoordinates]"){
             REQUIRE(decodeCoordinates(std::string(1,column)+std::to_string(row)) == DecodedCoordinates{static_cast<int>(column)-97,row});
         }
     }
-
     for(char column: {'A','B','C','D','E','F','G','H','I','J'})
     {
         for(int row: {1,2,3,4,5,6,7,8,9,10})
