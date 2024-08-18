e20cb8635defe37f41aebd232d25264aaa090e4e
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-18 18:13:33 +0200

    -

--- src/player/testPlayerConstructor.cpp
+++ src/player/testPlayerConstructor.cpp
@@ -5,15 +5,29 @@
 
 using namespace fleetBattle;
 
-TEST_CASE("","[]")
+TEST_CASE("Player constructor","[testPlayerConstructor]")
 {
-    
+    std::vector<ShipPosition> ships {   ShipPosition{{0,0},{0,1}}, //size 2
+                                        ShipPosition{{1,4},{3,4}}, //size 3
+                                        ShipPosition{{9,5},{9,8}}, //size 4
+                                        ShipPosition{{0,1},{0,5}}, //size 5
+                                        ShipPosition{{3,6},{8,6}}  //size 6
+                                    };
+    player playerA {ships};
+
+    CHECK(true);
 }
 
-TEST_CASE("","[!benchmark]")
+TEST_CASE("Player constructor benchmark","[!benchmark]")
 {
-    BENCHMARK("")
+    std::vector<ShipPosition> ships {   ShipPosition{{0,0},{0,1}}, //size 2
+                                        ShipPosition{{1,4},{3,4}}, //size 3
+                                        ShipPosition{{9,5},{9,8}}, //size 4
+                                        ShipPosition{{0,1},{0,5}}, //size 5
+                                        ShipPosition{{3,6},{8,6}}  //size 6
+                                    };
+    BENCHMARK("Player constructor")
     {
-
+        return player(ships);
     };
 }
