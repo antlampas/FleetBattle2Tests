81e407609d9b0752fcf5da4d4364e637aa8bbbc3
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-18 20:35:35 +0200

    -

--- src/player/testAddSunkShips.cpp
+++ src/player/testAddSunkShips.cpp
@@ -5,7 +5,7 @@
 
 using namespace fleetBattle;
 
-TEST_CASE("","[]")
+TEST_CASE("Add sunk ships","[testAddSunkShips]")
 {
     std::vector<ShipPosition> ships {   ShipPosition{{0,0},{0,1}}, //size 2
                                         ShipPosition{{1,4},{3,4}}, //size 3
@@ -13,11 +13,35 @@ TEST_CASE("","[]")
                                         ShipPosition{{0,1},{0,5}}, //size 5
                                         ShipPosition{{3,6},{8,6}}  //size 6
                                     };
+    
+    player playerA{ships};
+
+    CHECK(playerA.getSunkShipsCount() == 0);
+
+    playerA.checkShoot(std::make_pair(0,0));
+    playerA.checkShoot(std::make_pair(0,1));
+
+    CHECK(playerA.getSunkShipsCount() == 1);
+
+    playerA.checkShoot(std::make_pair(1,4));
+    playerA.checkShoot(std::make_pair(2,4));
+    playerA.checkShoot(std::make_pair(3,4));
+
+    CHECK(playerA.getSunkShipsCount() == 2);
 }
 
-TEST_CASE("","[!benchmark]")
+TEST_CASE("Add sunk ships benchmark","[!benchmark]")
 {
-    BENCHMARK("")
+    std::vector<ShipPosition> ships {   ShipPosition{{0,0},{0,1}}, //size 2
+                                        ShipPosition{{1,4},{3,4}}, //size 3
+                                        ShipPosition{{9,5},{9,8}}, //size 4
+                                        ShipPosition{{0,1},{0,5}}, //size 5
+                                        ShipPosition{{3,6},{8,6}}  //size 6
+                                    };
+    
+    player playerA{ships};
+    
+    BENCHMARK("Add sunk ships")
     {
 
     };
--- src/player/testIsShipSunk.cpp
+++ src/player/testIsShipSunk.cpp
@@ -32,6 +32,15 @@ TEST_CASE("Is ship sunk","[testIsShipSunk]")
 
 TEST_CASE("Is ship sunk benchmark","[!benchmark]")
 {
+    std::vector<ShipPosition> ships {   ShipPosition{{0,0},{0,1}}, //size 2
+                                        ShipPosition{{1,4},{3,4}}, //size 3
+                                        ShipPosition{{9,5},{9,8}}, //size 4
+                                        ShipPosition{{0,1},{0,5}}, //size 5
+                                        ShipPosition{{3,6},{8,6}}  //size 6
+                                    };
+    
+    player playerA{ships};
+    
     BENCHMARK("Is ship sunk")
     {
 
