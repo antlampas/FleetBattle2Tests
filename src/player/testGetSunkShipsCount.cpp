cde7d73056e83b35da85169774dae352b4293d02
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-18 13:41:09 +0200

    -

---
+++ src/player/testGetSunkShipsCount.cpp
@@ -0,0 +1,19 @@
+#include "catch2/catch_test_macros.hpp"
+#include "catch2/benchmark/catch_benchmark.hpp"
+
+#include "player.hpp"
+
+using namespace fleetBattle;
+
+TEST_CASE("","[]")
+{
+    
+}
+
+TEST_CASE("","[!benchmark]")
+{
+    BENCHMARK("")
+    {
+
+    };
+}
