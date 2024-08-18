6764e3b489bdc865121b75a0c87403cea49e1b60
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-18 18:44:28 +0200

    -

--- src/player/testCheckShoot.cpp
+++ src/player/testCheckShoot.cpp
@@ -10,8 +10,8 @@ TEST_CASE("Check shoot","[testCheckShoot]")
     std::vector<ShipPosition> ships {   ShipPosition{{0,0},{0,1}}, //size 2
                                         ShipPosition{{1,4},{3,4}}, //size 3
                                         ShipPosition{{9,5},{9,8}}, //size 4
-                                        ShipPosition{{0,1},{0,5}}, //size 5
-                                        ShipPosition{{3,6},{8,6}}  //size 6
+                                        ShipPosition{{5,0},{5,4}}, //size 5
+                                        ShipPosition{{0,9},{5,9}}  //size 6
                                     };
     BoardType board{makeBoardFromShipsList(ships)};
 
@@ -27,8 +27,8 @@ TEST_CASE("Check shoot benchmark","[!benchmark]")
     std::vector<ShipPosition> ships {   ShipPosition{{0,0},{0,1}}, //size 2
                                         ShipPosition{{1,4},{3,4}}, //size 3
                                         ShipPosition{{9,5},{9,8}}, //size 4
-                                        ShipPosition{{0,1},{0,5}}, //size 5
-                                        ShipPosition{{3,6},{8,6}}  //size 6
+                                        ShipPosition{{5,0},{5,4}}, //size 5
+                                        ShipPosition{{0,9},{5,9}}  //size 6
                                     };
     BoardType board{makeBoardFromShipsList(ships)};
 
