abdf06167b6758d8cfad574609421bba43678a91
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-19 21:19:33 +0200

    -

--- src/utilities/testTokenizeMessage.cpp
+++ src/utilities/testTokenizeMessage.cpp
@@ -7,8 +7,20 @@ using namespace fleetBattle;
 
 TEST_CASE("Tokenize message","[testTokenizeMessage]")
 {
-    std::string               message          {"Message Tokenizer"};
-    std::vector<std::string>> tokenizedMessage {"Message","Tokenizer"};
+    std::string              message          {"Message Tokenizer"};
+    std::vector<std::string> tokenizedMessage {"Message","Tokenizer"};
+
+    CHECK(tokenizeMessage(message) == tokenizedMessage);
+
+    message.clear();
+    tokenizedMessage.clear();
+
+    message = "Message Tokenizer again and again";
+    tokenizedMessage.push_back("Message");
+    tokenizedMessage.push_back("Tokenizer");
+    tokenizedMessage.push_back("again");
+    tokenizedMessage.push_back("and");
+    tokenizedMessage.push_back("again");
 
     CHECK(tokenizeMessage(message) == tokenizedMessage);
 }
